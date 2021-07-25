5d958bd1d27201729b98c2505f6785c9466c9946
Sotiris Moustogiannis <smoustogiannis@konnektable.com>
2021-07-25 18:19:29 +0300

    connect tkinter with django

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import View
 from .forms import UserRegisterForm
+import pickle
+from datasketch import MinHash, MinHashLSHForest
+import time
+import numpy as np
+from tkinter import filedialog
+import pandas as pd
 
 
 def register(request):
@@ -18,4 +26,44 @@ def register(request):
 
 @login_required
 def profile(request):
+    return render(request,'users/profile.html')
+
+#-----------------------------------------------------------------------------------------------------
+
+def preprocess(text):
+    tokens = text.lower()
+    tokens = tokens.split()
+    return tokens
+
+def predict(request):
+    text = 'googlesearchservice'
+
+    pickle_in1 = open("dict.pickledb", "rb")
+    database = pickle.load(pickle_in1)
+
+    perms = 256
+    num_results = 10
+
+    start_time = time.time()
+
+    print(start_time)
+
+    pickle_in = open("dict.pickle", "rb")
+    example_dict = pickle.load(pickle_in)
+
+    tokens = preprocess(text)
+    m = MinHash(num_perm=perms)
+    for s in tokens:
+        m.update(s.encode('utf8'))
+
+    idx_array = np.array(example_dict.query(m, num_results))
+    if len(idx_array) == 0:
+        return None # if your query is empty, return none
+
+    result = database.iloc[idx_array]['Service Name'] 
+
+    print('It took %s seconds to build forest.' %(time.time()-start_time))
+
+    print(result)
+
     return render(request,'users/profile.html')
