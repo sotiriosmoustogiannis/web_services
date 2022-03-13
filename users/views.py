f1ec25e1a40ba1b4b080bc4f3776bdfe1f737a85
“sotirios.moustogiannis” <smoustogiannis@konnektable.com>
2022-03-13 04:11:06 +0200

    update profile

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import View
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 import pickle
 from datasketch import MinHash, MinHashLSHForest
 import time
@@ -26,7 +27,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {
+        'u_form': u_form
+    }
+    return render(request,'users/profile.html', context)
 
 
 
